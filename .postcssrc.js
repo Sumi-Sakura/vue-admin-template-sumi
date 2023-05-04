31aec2f3a305a122dbee297bb056e1361dbbd225
sumi <1228668328@qq.com>
2023-05-04 23:23:03 +0800

    feat: 修改postcss配置文件名称

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,8 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  'plugins': {
+  plugins: {
     // 浏览器兼容 编辑目标浏览器：使用 package.json 中的 browserslist 字段
-    'autoprefixer': {},
+    autoprefixer: {},
   },
 }
