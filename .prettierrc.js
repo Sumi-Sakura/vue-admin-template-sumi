c0073f10fc27cc7ed2e0c9405a29d0fd92eac1b6
sumi <1228668328@qq.com>
2023-05-04 23:24:33 +0800

    feat: 修改prettier配置文件名称

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 单行长度
-  printWidth: 120,
+  printWidth: 100,
   // 缩进长度
   tabWidth: 2,
   // 使用空格代替tab缩进
@@ -16,5 +16,13 @@ module.exports = {
   // 在对象前后添加空格
   bracketSpacing: true,
   // 在 ES5 中有效的尾后逗号（如对象与数组等）
-  trailingComma: 'es5',
+  trailingComma: 'all',
+  // 箭头函数无论有无参数都加括号
+  arrowParens: 'always',
+  // 设置统一的行结尾样式(/n)
+  endOfLine: 'lf',
+  // 多行HTML (HTML, JSX, Vue, Angular)元素的>放在最后一行的末尾，而不是单独放在下一行(不适用于自关闭元素)
+  bracketSameLine: true,
+  // 与bracketSameLine配合一起设置
+  htmlWhitespaceSensitivity: 'ignore',
 }
